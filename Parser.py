ab4f964dc53d282e1a9314672329b3b1621332d3
Satish Bhambri <sbhambri@asu.edu>
2018-01-15 22:29:34 -0700

    Parser Code

---
+++ Parser.py
@@ -0,0 +1,6 @@
+import utils.nlp_parser as NLPParser
+
+def parse_query_text(query_string):
+    noun_phrases = NLPParser.get_noun_phrases(query_string)
+    named_entities = NLPParser.get_named_entities(query_string)
+    return noun_phrases, named_entities
